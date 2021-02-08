d0889579a3199f0cb93adad8d643a275d692e964
Rafael Melo <rafaelnmelo@live.com>
2021-02-08 10:25:02 -0300

    Criando CollectionReusableView

---
+++ The Movies/Movies/Utils/CollectionReusableView/CollectionReusableViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class CollectionReusableViewController: UICollectionReusableView {
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+}
